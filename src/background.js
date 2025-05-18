deff2900c11a3577d86060b0653b8fcb6b4e7c92
Matt Krueger <mkrueger@rstms.net>
2025-05-18 02:03:25 -0600

    fmt

--- src/background.js
+++ src/background.js
@@ -1607,7 +1607,7 @@ async function autoOpen() {
 async function onLoad() {
     try {
         console.warn("onLoad");
-	await initAPIKeys();
+        await initAPIKeys();
         await autoOpen();
     } catch (e) {
         console.error(e);
